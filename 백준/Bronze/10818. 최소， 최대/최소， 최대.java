df03572e834fe8435b78b25dafaaaee511297d89
sungjae <92013056+rlawo32@users.noreply.github.com>
2026-01-16 14:29:16 +0900

    [Bronze III] Title: 최소, 최대, Time: 496 ms, Memory: 93148 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -9,7 +9,12 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int i = 0;
         while (st.hasMoreTokens()) { arr[i] = Integer.parseInt(st.nextToken()); i++; }
-        Arrays.sort(arr);
-        System.out.print(arr[0] + " " + arr[n-1]);
+        int max = arr[0];
+        int min = arr[0];
+        for(int j=0; j<arr.length; j++) {
+            if(max < arr[j]) max = arr[j];
+            if(min > arr[j]) min = arr[j];
+        }
+        System.out.print(min + " " + max);
     }
 }
