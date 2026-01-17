be30ae4f09f524c53e9a02bdf4636bc4d49a02b6
sungjae <92013056+rlawo32@users.noreply.github.com>
2026-01-18 01:50:55 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 96 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,23 +1,22 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int[] arr = new int[30];
-        
-        for(int i=0; i<28; i++) {
-            int a = Integer.parseInt(br.readLine());
-            arr[a-1] = a;
+        ArrayList<Integer> list = new ArrayList<>();
+        String line;
+        while ((line = br.readLine()) != null) {
+            list.add(Integer.parseInt(line));
         }
-        
-        br.close();
-        
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i] == 0) { bw.write((i+1)+"\n"); }
+        Collections.sort(list);
+        StringBuilder sb = new StringBuilder();
+        int idx = 0;
+        for(int i=1; i<=30; i++) {
+            if(!list.contains(i)) {
+                sb.append(i).append("\n");
+            }
         }
-        
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
     }
 }
