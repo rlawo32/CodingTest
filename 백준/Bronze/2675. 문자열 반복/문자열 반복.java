0de0583718f337f435916a09a0f4e2e0ed9586ef
sungjae <92013056+rlawo32@users.noreply.github.com>
2026-01-19 17:29:59 +0900

    [Bronze II] Title: 문자열 반복, Time: 100 ms, Memory: 14000 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,26 +1,22 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args) throws Exception {
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int len = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        for(int i=0; i<len; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken());
-            String b = st.nextToken();
-            String res = "";
-            for(int j=0; j<b.length(); j++) {
-                for(int k=0; k<a; k++) {
-                    res += b.substring(j, j+1);
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int m = Integer.parseInt(st.nextToken());
+            String s = st.nextToken();
+            for(int j=0; j<s.length(); j++) {
+                for(int k=0; k<m; k++) {
+                    sb.append(s.substring(j, j+1));
                 }
             }
-            bw.write(res+"\n");
+            sb.append("\n");
         }
-        br.close();
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
     }
 }
