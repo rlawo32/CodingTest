1ea07b76e21f0a79423ad866e644364021f115f0
sungjae <92013056+rlawo32@users.noreply.github.com>
2026-01-19 10:50:57 +0900

    [Bronze I] Title: 평균, Time: 104 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,19 +1,26 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         double[] arr = new double[n];
-        double m = 0;
         int idx = 0;
-        while (st.hasMoreTokens()) { arr[idx] = Integer.parseInt(st.nextToken()); idx++; }
-        Arrays.sort(arr);
-        for(int i=0; i<arr.length; i++) {
-            m += arr[i] / arr[n-1] * 100;
+        int max = 0;
+        while (st.hasMoreTokens()) {
+            int score = Integer.parseInt(st.nextToken());
+            arr[idx] = score;
+            if(score > max) {
+                max = score;
+            }
+            idx++;
         }
-        System.out.print(m/n);
+        double result = 0;
+        for(int i=0; i<arr.length; i++) {
+            result += arr[i] / max * 100;
+        }
+        System.out.print(result/n);
     }
 }
