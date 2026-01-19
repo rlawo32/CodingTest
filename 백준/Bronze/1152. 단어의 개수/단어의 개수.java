1afe701254716738b5ad932dbce5732be66b938a
sungjae <92013056+rlawo32@users.noreply.github.com>
2026-01-19 17:48:32 +0900

    [Bronze II] Title: 단어의 개수, Time: 232 ms, Memory: 20064 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,15 +1,10 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args) throws Exception {
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        br.close();
-        
-        bw.write(st.countTokens()+"");
-        bw.flush();
-        bw.close();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.print(st.countTokens());
     }
 }
