36ab05975b1e79d69ca2202196e7753f10e6e155
sungjae <92013056+rlawo32@users.noreply.github.com>
2026-01-19 16:48:22 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 124 ms, Memory: 15908 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,22 +1,31 @@
 import java.io.*;
+import java.util.*;
 
-public class Main{
-    public static void main(String[] args) throws Exception {
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        char[] arr = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
-                      'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-        String S = br.readLine();
-        br.close();
-        for(char c : arr) {
-            int i = S.indexOf(c+"");
-            if(i != -1) {
-                bw.write(i+" ");
-            } else {
-                bw.write("-1 ");
+        String s = br.readLine();
+        String[] arr = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};        
+        for(int i=0; i<s.length(); i++) {
+            String str = s.substring(i, i+1);
+            for(int j=0; j<arr.length; j++) {
+                if(arr[j].equals(str)) {
+                    arr[j] = "*"+i;
+                    break;
+                }
             }
         }
-        bw.flush();
-        bw.close();
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i].length() > 1) {
+                arr[i] = arr[i].substring(1);
+            } else {
+                arr[i] = "-1";
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<arr.length; i++) {
+            sb.append(arr[i] + " ");
+        }
+        System.out.print(sb);
     }
 }
