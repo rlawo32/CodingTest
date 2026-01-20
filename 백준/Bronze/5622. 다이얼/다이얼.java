84f7fef8b88fb770f58129554a647769ffe26709
sungjae <92013056+rlawo32@users.noreply.github.com>
2026-01-20 17:34:19 +0900

    [Bronze II] Title: 다이얼, Time: 96 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -1,29 +1,20 @@
 import java.io.*;
-import java.util.*;
 
-public class Main{
-    public static void main(String[] args) throws Exception {
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String S = br.readLine();
-        br.close();
-        char[] arr = S.toCharArray();
-        int time = 0;
-        
-        for(int i=0; i<arr.length; i++) {
-            int c = arr[i];
-			if(c>=65 && c<=67) time += 3;
-			else if(c>=68 && c<=70) time +=4;
-			else if(c>=71 && c<=73) time +=5;
-			else if(c>=74 && c<=76) time +=6;
-			else if(c>=77 && c<=79) time +=7;
-			else if(c>=80 && c<=83) time +=8;
-			else if(c>=84 && c<=86) time +=9;
-			else if(c>=87 && c<=90) time +=10;
+        String s = br.readLine();
+        String[] arr = {"ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"};
+        int r = 0;
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            for(int j=0; j<arr.length; j++) {
+                if(arr[j].indexOf(c) != -1) {
+                    r += j+3;
+                    break;
+                }
+            }
         }
-        
-        bw.write(time+"");
-        bw.flush();
-        bw.close();
+        System.out.print(r);
     }
 }
