37cf95171a5f1cb3d9293ee6ef2d750239be61fb
sungjae <92013056+rlawo32@users.noreply.github.com>
2026-01-21 15:50:43 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 13856 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.java
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("         ,r'\"7");
-        System.out.println("r`-_   ,'  ,/");
-        System.out.println(" \\. \". L_r'");
-        System.out.println("   `~\\/");
-        System.out.println("      |");
-        System.out.println("      |");
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r'\"7").append("\n");
+        sb.append("r`-_   ,'  ,/").append("\n");
+        sb.append(" \\. \". L_r'").append("\n");
+        sb.append("   `~\\/").append("\n");
+        sb.append("      |").append("\n");
+        sb.append("      |").append("\n");
+        System.out.print(sb);
     }
 }
