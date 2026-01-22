ab6581a018f7805b76af6cee763ff1d90e806d9f
sungjae <92013056+rlawo32@users.noreply.github.com>
2026-01-22 16:41:17 +0900

    [Bronze I] Title: 단어 공부, Time: 464 ms, Memory: 112032 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,47 +1,25 @@
 import java.io.*;
-import java.lang.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        char[] arr = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-        int[] arr2 = new int[26];
-        String str = br.readLine().toLowerCase();
-        String res = "?";
-        int idx = 0;
-        
-        for(char c : arr) {
-            int len1 = str.length();
-            if(len1 < 1) {
-                break;
+        String s = br.readLine();
+        Map<String, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            String r = String.valueOf(c).toUpperCase();
+            map.put(r, map.getOrDefault(r, 0) + 1);
+        }    
+        String result = "";
+        int max = 0;
+        for(Map.Entry<String, Integer> entry : map.entrySet()) {
+            if(entry.getValue() > max) {
+                max = entry.getValue();
+                result = entry.getKey();
+            } else if(entry.getValue() == max) {
+                result = "?";
             }
-            int len2 = str.replace(String.valueOf(c), "").length();
-            arr2[idx++] = len1 - len2;
         }
-        
-        int max = -1;
-        for(int i=0; i<arr.length; i++) {
-            if(arr2[i] > max) {
-                max = arr2[i];
-				idx = i;
-            } 
-        }
-
-		for(int i=0; i<arr.length; i++) {
-            if(idx != i && arr2[i] == max) {
-				max = -1;
-                break;
-            } 
-        }
-
-		if(max > 0) {
-			res = (arr[idx]+"").toUpperCase();
-		}  
-        
-        bw.write(res);
-        
-        bw.flush();
-        bw.close();
+        System.out.print(result);
     }
 }
