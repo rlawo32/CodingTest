0b0bef7af00d0fcd2aeee7b3197e6ac101f727ff
sungjae <92013056+rlawo32@users.noreply.github.com>
2026-01-27 17:40:11 +0900

    [Silver V] Title: 너의 평점은, Time: 96 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -1,50 +1,33 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-
-		double result = 0, credit_sum = 0; // result = 학점*과목평균, credit_sum = 학점의 총합
-		char c;
-				
-		for(int i=0;i<20;i++) {
-			double credit = 0; // 학점
-			double grade = 0; // 등급에 따른 점수
-			
-			String str_arr[] = sc.nextLine().split(" ");
-			credit += Double.parseDouble(str_arr[1]);
-			c = str_arr[2].charAt(0);
-			
-			switch(c) {
-			case 'A':
-				if(str_arr[2].charAt(1) == '+') grade += 4.5;
-				else grade += 4.0;
-				break;
-			case 'B':
-				if(str_arr[2].charAt(1) == '+') grade += 3.5;
-				else grade += 3.0;
-				break;
-			case 'C':
-				if(str_arr[2].charAt(1) == '+') grade += 2.5;
-				else grade += 2.0;
-				break;
-			case 'D':
-				if(str_arr[2].charAt(1) == '+') grade += 1.5;
-				else grade += 1.0;
-				break;
-			case 'F':
-				grade += 0;
-				break;
-			case 'P':
-				grade += 0;
-				credit = 0;
-				break;
-			}
-			credit_sum += credit;
-			result += credit*grade;
-		}
-		sc.close();
-
-		System.out.println(result/credit_sum);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double result = 0;
+        double total = 0;
+        String line;
+        while ((line = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(line);
+            String subject = st.nextToken();
+            double score = Double.parseDouble(st.nextToken());
+            String grade = st.nextToken();            
+            switch(grade) {
+                case "A+": result += score * 4.5; break;
+                case "A0": result += score * 4; break;
+                case "B+": result += score * 3.5; break;
+                case "B0": result += score * 3; break;
+                case "C+": result += score * 2.5; break;
+                case "C0": result += score * 2; break;
+                case "D+": result += score * 1.5; break;
+                case "D0": result += score * 1; break;
+                case "F": result += score * 0; break;
+                default: result += 0; break;
+            }
+            if(!grade.equals("P")) {
+                total += score;
+            }
+        }
+        System.out.print(result/total);
+    }
 }
