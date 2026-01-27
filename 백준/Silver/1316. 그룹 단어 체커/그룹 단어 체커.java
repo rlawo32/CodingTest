f0e758e3f12bfcff98e1eb4b3cddeb09beba2d90
sungjae <92013056+rlawo32@users.noreply.github.com>
2026-01-27 17:15:40 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 108 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,35 +1,26 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        char[] arr1 = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-        int len = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         int cnt = 0;
-        for(int i=0; i<len; i++) {
-            int[] arr2 = new int[26];
-            String str = br.readLine();
-			char c = '!';
-            Loop1 : for(int j=0; j<str.length(); j++) {
-                if(c != str.charAt(j)) {
-                    c = str.charAt(j);
-                    for(int k=0; k<arr1.length; k++) {
-                        if(c == arr1[k]) {
-                            if(arr2[k] > 0) {
-                                cnt += 1;
-                                break Loop1;
-                            } else {
-                                arr2[k] = 1;   
-                            }
-                        }
-                    }
+        for(int i=0; i<n; i++) {
+            Map<String, Integer> map = new HashMap<>();
+            String s = br.readLine();
+            for(int j=0; j<s.length(); j++) {
+                String t = String.valueOf(s.charAt(j));
+                if(map.containsKey(t)) {
+                    int x = map.get(t);
+                    if((j-x)>1) {
+                        cnt++;
+                        break;   
+                    }              
                 }
-	        }
+                map.put(t, j);
+            }
         }
-        br.close();
-        bw.write(len-cnt+"");
-        bw.flush();
-        bw.close();
+        System.out.print(n-cnt);
     }
 }
