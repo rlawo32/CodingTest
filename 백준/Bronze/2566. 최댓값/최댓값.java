c05536aefc29d6e306912058e961147ff7c994f6
sungjae <92013056+rlawo32@users.noreply.github.com>
2026-01-29 17:30:25 +0900

    [Bronze III] Title: 최댓값, Time: 108 ms, Memory: 16096 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -2,33 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        int[][] arr = new int[9][9];
-        for(int i=0; i<9; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for(int j=0; j<9; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        br.close();
+        String line;
+        int n = 0;
         int max = 0;
-        int row = 0;
-        int col = 0;
-        for(int i=0; i<9; i++) {
-            for(int j=0; j<9; j++) {
-                if(arr[i][j] >= max) {
-                    max = arr[i][j];
-                    row = i+1;
-                    col = j+1;
+        String s = "";
+        while ((line = br.readLine()) != null) {
+            int m = 0;
+            n++;
+            StringTokenizer st = new StringTokenizer(line);
+            while (st.hasMoreTokens()) {
+                m++;
+                int i = Integer.parseInt(st.nextToken());
+                if(i >= max) {
+                    max = i;
+                    s = n + " " + m;
                 }
             }
         }
-        
-        bw.write(max+"\n"+row+" "+col);
-        bw.flush();
-        bw.close();
+        StringBuilder sb = new StringBuilder();
+        sb.append(max).append("\n").append(s);
+        System.out.print(sb);
     }
 }
